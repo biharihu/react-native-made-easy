3b81a816fa5570f946345d686462349890755bea
Akash Kumar <53407198+biharihu@users.noreply.github.com>
2020-05-06 00:08:23 +0530

    component

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Card from "./Card";
+import { height, width } from "./dimensions";
+import getStatusBarHeight from "./getStatusbarHeight";
+
+module.exports = {
+  Card,
+  height,
+  width,
+  getStatusBarHeight,
+};
